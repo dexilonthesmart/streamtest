c364a3959ddd0ee134311e452003807a69042621
dexilonthesmart <kene7914@gmail.com>
2024-07-17 15:38:25 -0700

    page1.py

--- page1.py
+++ page1.py
@@ -4,6 +4,3 @@ st.title("# Team it works")
 
 st.write("# Surprise 👋")
 
-# Group by input anger level and calculate the mean fear for each level
-grouped_df = df.groupby('input_fear_ordinal')['output_anger_ordinal'].mean().reset_index()
-
